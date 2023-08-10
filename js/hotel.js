ae842d6c03a2f1af7f8a55e7400b794597cb3ab5
Pranali Dongare <139345001+dongarepranali@users.noreply.github.com>
2023-08-10 17:28:16 +0530

    website complete

--- js/hotel.js
+++ js/hotel.js
@@ -2,4 +2,19 @@ var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
   });
-  
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
